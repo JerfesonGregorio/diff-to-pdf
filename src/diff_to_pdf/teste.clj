c719d1fbd462cd40b9c5ebd6cb69410ad4cfb006
JerfesonGregorio <pro.jerfeson@gmail.com>
2024-07-18 11:03:59 -0300

    iniciando contagem de linhas corretamente

--- src/diff_to_pdf/teste.clj
+++ src/diff_to_pdf/teste.clj
@@ -8,12 +8,13 @@
   (slurp file-name))
 
 (defn read-file-obligation [file-name]
-  (slurp (io/reader file-name :encoding "ISO-8859-1"))) ; Leitura de arquivo em encoding para que o clj-pdf reconheça os símbolos estranhos
+  (slurp (io/reader file-name :encoding "ISO-8859-1")))     ; Leitura de arquivo em encoding para que o clj-pdf reconheça os símbolos estranhos
 
 (def modelo (read-file-model "src/modelo.txt"))
 (def obrigacao (read-file-obligation "src/obrigacao.txt"))
 
 (def diffs (d/diff modelo obrigacao ::d/cleanup ::d/cleanup-semantic))
+
 
 ;----------------------------------------------------------------------------------------------------
 
@@ -30,6 +31,20 @@
     [0 0 0]))                                               ; preto como padrão (cor da fonte)
 
 
+(defn count-newlines [s]
+  (count (re-seq #"(?m)^\|[a-zA-Z0-9]\d{3}\|" s)))
+
+(defn count-inserted-lines [diffs]
+  (reduce + 0 (for [text diffs]
+                (count-newlines text))))
+
+(defn count-deleted-lines [diffs]
+  (reduce + 0 (for [text diffs]
+                (count-newlines text))))
+
+(defn calculate-line-difference [insert delete]
+  (- (count-inserted-lines insert) (count-deleted-lines delete)))
+
 (defn diff-insert [diffs]
   (let [combined-chunks (for [{:keys [plumula.diff/operation plumula.diff/text]} diffs]
                           (case operation
@@ -40,13 +55,16 @@
     (concat [[:paragraph combined-chunks]])))               ; Retorna o trecho original e o que foi inserido (diff verde)
 
 
+(def abs (atom 0))
 
 
-(defn count-newlines [s]
-    (re-matches #"\n\|[a-zA-Z0-9]\d{3}\|" s)) ; Padrão a identificar para inserção de linhas em branco
+(defn incrementar-atom []
+  (swap! abs inc)
+  nil)
 
+(reset! abs 0)
 
-
+@abs
 
 (defn diff-delete [diffs]
   (let [combined-chunks (for [{:keys [plumula.diff/operation plumula.diff/text]} diffs]
@@ -54,11 +72,11 @@
                             :plumula.diff/equal [:chunk text]
                             :plumula.diff/delete [:chunk {:background (operation-background-color operation)
                                                           :color      (operation-text-color operation)} text]
-                            :plumula.diff/insert [:chunk {:background [0 0 0]} (apply str (repeat (count-newlines text) "\n"))]))] ; Local em que a lógica de novas linhas será utilizada
+                            :plumula.diff/insert (do [:chunk {:background [0 0 0]} ] (incrementar-atom)) ))] ; Local em que a lógica de novas linhas será utilizada
     (concat [[:paragraph combined-chunks]])))               ; Retorna o trecho original e o que foi deletado (diff vermelho)
 
 (defn format! [arg]
-  (mapcat identity (into [] arg))) ; Transforma a lazy-seq em um vetor e reduz um nível de aninhamento para que a lib clj-pdf possa reconhecê-lo como um parágrafo
+  (mapcat identity (into [] arg)))                          ; Transforma a lazy-seq em um vetor e reduz um nível de aninhamento para que a lib clj-pdf possa reconhecê-lo como um parágrafo
 
 
 (defn diff-to-pdf [model obligation title file-name]
@@ -78,9 +96,52 @@
 
 
 
+
+
+
+(def lazy-seq-example
+  (lazy-seq (range 10)))
+
+(def result1 (doall lazy-seq-example))                      ; Consumindo a sequência pela primeira vez
+(def result2 (doall lazy-seq-example))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 (comment
   (type (into [] (diff-insert diffs)))
   (diff-delete diffs)
   (into [] (diff-delete diffs))
   (count-newlines "\n|c222|")
+
+  (defn count-pattern [s]
+    (count (re-seq #"(?m)^\|[a-zA-Z0-9]\d{3}\|" s)))
+
+  (def input-string "
+
+")
+
+  (count-pattern input-string)
+
+  (count-deleted-lines insert)
+  (count-inserted-lines insert)
+  (calculate-line-difference m o)
   )
